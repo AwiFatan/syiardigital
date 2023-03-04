e45e4ca2a321899e7f4e814ff8fd220dfc5decc3
Awi Fatan <59038888+AwiFatan@users.noreply.github.com>
2023-03-04 20:27:11 +0700

    Update fisika.js

--- fisika.js
+++ fisika.js
@@ -166,7 +166,7 @@
                         var iframe =`
                         <div class='text-center'>
                             <div class="video">
-                                <iframe width="360" height="768"
+                                <iframe width="180" height="640"
                                     src="`+srcyoutube+`" frameborder="1"
                                     allowfullscreen></iframe>
                             </div>
